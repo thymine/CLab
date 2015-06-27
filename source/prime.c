e8ccde3497d08ed6220ea734c3ee0c9e5c85576e
thymine <zxdeinstein@163.com>
2015-06-27 16:21:03 +0800

    recursion control

---
+++ source/prime.c
@@ -0,0 +1,36 @@
+/*************************************************************************
+	// File Name: prime.c
+	// Author: Zhang
+	// Created Time: 2015年06月27日 星期六 15时21分10秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 2;
+	int count = 0; // e.g. get the top 50 prime numbers
+	// for (x = 2; x < 100; x++)
+	// for (x = 2; count < 50; x++)
+	while (count < 50)
+	{
+		int i;
+		int isPrime = 1; // x is a prime number
+		for (i = 2; i < x; i++)
+		{
+			if (x % i == 0)
+			{
+				isPrime = 0; // x is not a prime number
+				break;
+			}
+		}
+		if (isPrime)
+		{
+			count++;
+			printf("%d ", x);
+		}
+		x++;
+	}
+	printf("\n");
+    return 0;
+}
