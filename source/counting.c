bc8d88db1cb07f1c57655998479e2fe05d0cb679
thymine <zxdeinstein@163.com>
2015-06-28 14:41:30 +0800

    initialize array

---
+++ source/counting.c
@@ -0,0 +1,36 @@
+/*************************************************************************
+	// File Name: counting.c
+	// Author: Zhang
+	// Created Time: 2015年06月28日 星期日 14时06分21秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	const int n = 10; // constant: array length
+	scanf("%d", &x);
+	int count[n];
+	// initinalize array
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		count[i] = 0;
+	}
+	while (x != -1) 
+	{
+		// counting
+		if (x >= 0 && x <=9)
+		{
+			count[x]++;
+		}
+		scanf("%d", &x);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d: %d\n", i, count[i]);
+	}
+
+    return 0;
+}
