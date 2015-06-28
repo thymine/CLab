6f3f7ee4a0ccbba46d39aaa7ac56759ef52071a0
thymine <zxdeinstein@163.com>
2015-06-28 14:40:43 +0800

    array declaration

--- source/avg.c
+++ source/avg.c
@@ -1,7 +1,6 @@
 /*************************************************************************
 	>> File Name: avg.c
 	>> Author: Zhang
-	>> Mail: zhanglts@163.com 
 	>> Created Time: 2015年05月31日 星期日 20时37分55秒
  ************************************************************************/
 
@@ -9,9 +8,53 @@
 
 int main()
 {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	double c = (a + b) / 2.0;
-	printf("Avg of %d and %d is %f\n", a, b, c);
+	double sum = 0.0;
+	int x;
+	int count = 0;
+	int numbers[100];
+	scanf("%d", &x);
+	while(x != -1)
+	{
+		numbers[count] = x;
+		// debug start:
+		{
+			printf("%d ", count);
+			int i;
+			for (i = 0; i <= count; i++)
+			{
+				printf("%d", numbers[i]);
+				if (i < count)
+				{
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+		// debug end.
+		sum += x;
+		count++;
+		scanf("%d", &x);
+	}
+	if (count > 0)
+	{
+		double avg = sum / count;
+		printf("%f\n", avg);
+		int i;
+		for (i = 0; i < count; i++)
+		{
+			if (numbers[i] > avg)
+			{
+				printf("%d", numbers[i]);
+				if (i < count - 1)
+				{
+					printf(" ");
+				}
+				else if (i < count)
+				{
+					printf("\n");
+				}
+			}
+		}
+	}
     return 0;
 }
