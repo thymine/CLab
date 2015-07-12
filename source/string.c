1b9af4d1d7d8461d22bd2d1cf79a42a83994438e
thymine <zxdeinstein@163.com>
2015-07-12 14:57:46 +0800

    字符串函数

--- source/string.c
+++ source/string.c
@@ -5,10 +5,83 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
+void e();
 void f();
+void g();
+void cmp();
+void cpy();
+void cat();
+void chr();
 
 int main()
+{
+	// e();
+	// f();
+	// g();
+	// cmp();
+	chr();
+	// cpy();
+	// cat();
+    return 0;
+}
+
+void chr()
+{
+	char x[] = "My name is Zhang! You can call me Z!";
+    char *p = strchr(x, 'Z');
+	printf("x = %s\n", x);
+	printf("p = %c\n", *p);
+	printf("x -> %p\n", &x);
+	printf("p -> %p\n", p);
+
+	char *rp = strrchr(x, 'Z');
+	printf("rp -> %p\n", rp);
+}
+
+void cat()
+{
+	char a[100] = "Hello";
+	char b[100] = "World";
+	// strcat(a, b);
+	strncat(a, b, 5);
+	printf("a = %s\n", a);
+	printf("b = %s\n", b);
+}
+
+void cpy()
+{
+	char m[] = "hello";
+	char n[20] = "world!";
+	// strcpy(n, m);
+	strncpy(n, m, 3);
+	printf("sizeof(n) = %lu\n", sizeof(n));
+	printf("n = %s\n", n);
+}
+
+void cmp()
+{
+	char a[] = "mnfzyy";
+	char b[] = "mob";
+	char c[] = "oom";
+	char d[] = "exit";
+	// printf("a <> b ? %d\n", strcmp(a, b));
+	// printf("a <> c ? %d\n", strcmp(a, c));
+	// printf("a <> d ? %d\n", strcmp(a, d));
+	printf("a <> b ? %d\n", strncmp(a, b, 3));
+	printf("a <> c ? %d\n", strncmp(a, c, 3));
+	printf("a <> d ? %d\n", strncmp(a, d, 3));
+}
+
+void g()
+{
+	char s[] = "Hello";
+	printf("strlen(s) = %lu\n", strlen(s));
+	printf("sizeof(s) = %lu\n", sizeof(s));
+}
+
+void e()
 {
 	printf("Hello, "
 			"world!\n");
@@ -26,9 +99,6 @@ int main()
 	s = t;
 	t[2] = 'm';
 	printf("s=%s\n", s);
-
-	f();
-    return 0;
 }
 
 void f()
@@ -43,3 +113,4 @@ void f()
 	char *d = "";
 	printf("d = %s\n", d);
 }
+
