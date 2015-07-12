7a7257b03d6f5c24e54dc9f9322a9077fb2dbc2d
thymine <zxdeinstein@163.com>
2015-07-12 15:23:43 +0800

    整数类型:char, short, int, long, long long

---
+++ source/integer.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	// File Name: integer.c
+	// Author: Zhang
+	// Created Time: 2015年07月12日 星期日 15时20分54秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("sizeof(char) = %lu\n", sizeof(char));
+	printf("sizeof(short) = %lu\n", sizeof(short));
+	printf("sizeof(int) = %lu\n", sizeof(int));
+	printf("sizeof(long) = %lu\n", sizeof(long));
+	printf("sizeof(long long) = %lu\n", sizeof(long long));
+    return 0;
+}
