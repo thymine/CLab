a9f66e7e16f3e3dadb5644f6822202e4ded27723
thymine <zxdeinstein@163.com>
2015-07-26 12:24:19 +0800

    在32位下编译available.c

--- source/available.c
+++ source/available.c
@@ -7,18 +7,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * build: gcc available.c -o available -m32
+ */
+void go();
+
 int main()
 {
+	go();
     return 0;
 }
 
 void go()
 {
 	int cnt = 0;
-	void* p;
+	void* p = NULL;
 	while (p = malloc(100 * 1024 * 1024))
 	{
 		cnt++;
 	}
 	printf("可以分配%d00Mb的空间\n", cnt);
+	free(p);
 }
