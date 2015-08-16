90ed50094ec352e928f3b0a02f40aeb5bf74f398
thymine <zxdeinstein@163.com>
2015-08-16 16:29:56 +0800

    枚举

--- source/enum.c
+++ source/enum.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+enum colors {RED, GREEN, BLUE, NumOfColors};
+
 int main(void)
 {
 	enum months {JAN = 1, FEB, MAR, APR, MAY, JUN, 
 			JUL, AUG, SEP, OCT, NOV, DEC};
 	printf("JAN = %d\n", JAN);
+
+	char *colorNames[NumOfColors] = {"red", "green", "blue"};
+	printf("%s\n", colorNames[1]);
 	return 0;
 }
