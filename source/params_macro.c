9605d497f8e90499ceab67dd3ef062ac8eaca019
thymine <zxdeinstein@163.com>
2015-09-03 18:01:12 +0800

    Recompile

--- source/params_macro.c
+++ source/params_macro.c
@@ -3,6 +3,9 @@
 #define RADTODEG(x)((x) * 57.29578)
 #define MIN(a,b)((a)>(b)?(b):(a))
 
+#define __PRT_AGE_BY_NAME(name, age) \
+	void print##name() {printf("%d\n", age);}
+
 int main(int argc, char const *argv[])
 {
 	int i;
@@ -10,5 +13,9 @@ int main(int argc, char const *argv[])
 	printf("cube(5) = %d\n", cube(i));
 	printf("RADTODEG(10) = %f\n", RADTODEG(10));
 	printf("MIN(13,2):%d\n", MIN(13,2));
+
+	__PRT_AGE_BY_NAME(Zhang, 22);
+
+	printZhang(17);
 	return 0;
 }
