c94efbb920ca3fba5b168867df0df416bb6e63c3
zhangxiangdong <zxdeinstein@163.com>
2017-03-10 14:54:13 +0800

    Invoke setUp first

--- python/xUnit.py
+++ python/xUnit.py
@@ -1,16 +1,23 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-        self.wasSetUp = None
+
+    def setUp(self):
+        pass
 
     def run(self):
+        self.setUp()
         exec "self." + self.name + "()"
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        self.wasSetUp = None
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
@@ -23,11 +30,17 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert (self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert (self.test.wasSetUp)
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
