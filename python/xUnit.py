608ba3998c48d976e025446cb4bba9aebe4354c7
zhangxiangdong <zxdeinstein@163.com>
2017-03-10 15:22:02 +0800

    Invoke tearDown afterwards

--- python/xUnit.py
+++ python/xUnit.py
@@ -8,39 +8,31 @@ class TestCase:
     def run(self):
         self.setUp()
         exec "self." + self.name + "()"
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
-        self.wasSetUp = None
         TestCase.__init__(self, name)
 
     def setUp(self):
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
 
-
-# test = WasRun("testMethod")
-# print test.wasRun
-# test.run()
-# print test.wasRun
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert (self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod tearDown " == test.log)
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
