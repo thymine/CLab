7cfde925d00055a5b2002087a951318cbe2be7e3
zhangxiangdong <zxdeinstein@163.com>
2017-04-16 14:47:15 +0800

    Repeat three times

--- python/takeABreak.py
+++ python/takeABreak.py
@@ -3,6 +3,12 @@ import time
 
 url = 'http://music.163.com/song?id=18003388'
 
-time.sleep(3)
-# Open URL in a new tab, if a browser window is already open.
-webbrowser.open_new_tab(url)
+total_breaks = 3
+break_count = 0
+print("This program started on " + time.ctime())
+while break_count < total_breaks:
+    time.sleep(3) # For development only
+    # time.sleep(2 * 60 * 60)
+    # Open URL in a new tab, if a browser window is already open.
+    webbrowser.open_new_tab(url)
+    break_count += 1
