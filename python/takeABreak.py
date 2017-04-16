fd4c533503f582265c51a2d0135bb699f82cfbc2
zhangxiangdong <zxdeinstein@163.com>
2017-04-16 14:17:53 +0800

    Make the program wait

--- python/takeABreak.py
+++ python/takeABreak.py
@@ -1,6 +1,8 @@
 import webbrowser
+import time
 
 url = 'http://music.163.com/#/song?id=18003388&userid=64488817'
 
+time.sleep(3)
 # Open URL in a new tab, if a browser window is already open.
 webbrowser.open_new_tab(url)
