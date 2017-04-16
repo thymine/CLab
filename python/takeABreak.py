1c4152b48d829bd3d4e1338ce1f338363dc4a811
zhangxiangdong <zxdeinstein@163.com>
2017-04-16 14:46:37 +0800

    Update song url

--- python/takeABreak.py
+++ python/takeABreak.py
@@ -1,7 +1,7 @@
 import webbrowser
 import time
 
-url = 'http://music.163.com/#/song?id=18003388&userid=64488817'
+url = 'http://music.163.com/song?id=18003388'
 
 time.sleep(3)
 # Open URL in a new tab, if a browser window is already open.
