fc6775466d26dbfcf0a5fe6c75c8dc2e95bc3445
zhangxiangdong <zxdeinstein@163.com>
2017-04-23 17:10:26 +0800

    Optimize code and draw a triangle

--- python/mindStorms.py
+++ python/mindStorms.py
@@ -1,26 +1,26 @@
 import turtle
 
-def draw_square():
+def main():
     window = turtle.Screen()
     window.bgcolor("red")
 
+    draw_square()
+    draw_circle()
+    draw_triangle()
+
+    window.exitonclick()
+
+def draw_square():
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow", "blue")
     brad.speed(2)
-    
-    brad.forward(100)
-    brad.right(90)
 
-    brad.forward(100)
-    brad.right(90)
+    for i in range(0, 4):
+        brad.forward(100)
+        brad.right(90)
 
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
+def draw_circle():
     angie = turtle.Turtle()
     angie.shape("turtle")
     angie.color("orange")
@@ -28,6 +28,19 @@ def draw_square():
 
     angie.circle(100)
 
-    window.exitonclick()
+def draw_triangle():
+    lucia = turtle.Turtle()
+    lucia.shape("turtle")
+    lucia.color("green")
+    lucia.speed("fast")
 
-draw_square()
+    lucia.forward(100)
+    lucia.right(120)
+
+    lucia.forward(100)
+    lucia.right(120)
+
+    lucia.forward(100)
+    lucia.right(120)
+
+main()
