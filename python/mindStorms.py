a237715b9ba49af2b0796e4d7e1fbcfb05fcfa73
zhangxiangdong <zxdeinstein@163.com>
2017-04-23 18:02:32 +0800

    Draw triangles

--- python/mindStorms.py
+++ python/mindStorms.py
@@ -1,13 +1,13 @@
 import turtle
 
-def main():
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
 
     brad = turtle.Turtle()  
     brad.shape("turtle")
     brad.color("yellow", "blue")
-    brad.speed(7)
+    brad.speed(10)
 
     step = 0
     while (step < 36):
@@ -16,7 +16,15 @@ def main():
         step += 1
     
     # draw_circle()
-    # draw_triangle()
+
+    lucia = turtle.Turtle()
+    lucia.shape("turtle")
+    lucia.color("blue")
+    lucia.speed("fast")
+
+    for i in range(0, 36):
+        draw_triangle(lucia)
+        lucia.right(10)
 
     window.exitonclick()
 
@@ -33,19 +41,9 @@ def draw_circle():
 
     angie.circle(100)
 
-def draw_triangle():
-    lucia = turtle.Turtle()
-    lucia.shape("turtle")
-    lucia.color("green")
-    lucia.speed("fast")
+def draw_triangle(lucia):
+    for i in range(0, 3):
+        lucia.forward(100)
+        lucia.right(120)
 
-    lucia.forward(100)
-    lucia.right(120)
-
-    lucia.forward(100)
-    lucia.right(120)
-
-    lucia.forward(100)
-    lucia.right(120)
-
-main()
+draw_art()
