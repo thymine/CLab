4f140aafefb1503493f87cce5015851aeff04b37
zhangxiangdong <zxdeinstein@163.com>
2017-07-02 11:55:27 +0800

    Check profanity

--- python/checkProfanity.py
+++ python/checkProfanity.py
@@ -1,9 +1,17 @@
 from os import path
+import urllib
 
 def read_text():
     quotes = open(path.relpath("movie_quotes.txt"))
     contents = quotes.read()
     print(contents)
     quotes.close()
+    check_profanity(contents)
 
+def check_profanity(text_to_check):
+    conn = urllib.urlopen("https://www.wdylike.appspot.com/?q=" + text_to_check)
+    result = conn.read()
+    print(result)
+    conn.close()
+    
 read_text()
