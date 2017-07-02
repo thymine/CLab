368895103f858bde15263e6d8c91b812b80c4cfb
zhangxiangdong <zxdeinstein@163.com>
2017-07-02 13:01:47 +0800

    Create another movie

--- python/entertainment_center.py
+++ python/entertainment_center.py
@@ -5,3 +5,9 @@ toy_story = media.Movie("Toy Story",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.storyline)
