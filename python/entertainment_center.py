feb4178df017bf8d5cba0118a564611cd9737c16
zhangxiangdong <zxdeinstein@163.com>
2017-07-08 09:13:34 +0800

    Build movie website

--- python/entertainment_center.py
+++ python/entertainment_center.py
@@ -1,14 +1,17 @@
-import media
+import media, fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
-print(toy_story.storyline)
+# print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-print(avatar.storyline)
-avatar.show_trailer()
+# print(avatar.storyline)
+# avatar.show_trailer()
+
+movies = [toy_story, avatar]
+fresh_tomatoes.open_movies_page(movies)
