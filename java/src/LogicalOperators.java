59f763d39d53b5e303bf1cda29598bd9c0427b68
zhangxiangdong <zxdeinstein@163.com>
2017-07-22 09:17:08 +0800

    Logical operators

---
+++ java/src/LogicalOperators.java
@@ -0,0 +1,15 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int i = 1;
+        if (i == 2 & i++ == 2) {
+
+        }
+        System.out.println("i = " + i);
+
+        int j = 3;
+        if (j == 2 && j++ == 3) {
+        
+        }   
+        System.out.println("j = " + j);
+    }
+}
