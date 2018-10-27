8d3dd88853f68918f0641ebfb34791beaf012130
zhangxiangdong <zxdeinstein@163.com>
2018-10-27 17:02:12 +0800

    Read user input

---
+++ cpp/userinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name = "";
+	cout << "What's your name? " << endl;
+	cin >> name;
+	cout << "Hello " << name << "!" << endl;
+	return 0;
+}
