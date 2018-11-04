05a5f83acffc4f9f1344ba6b5cfe6c00167a5d2a
zhangxiangdong <zxdeinstein@163.com>
2018-11-04 09:41:50 +0800

    Use stringstream

--- cpp/userinput.cpp
+++ cpp/userinput.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-	string name = "";
-	cout << "What's your name? " << endl;
-	cin >> name;
-	cout << "Hello " << name << "!" << endl;
-	return 0;
+    string sLength, sWidth;
+    float length = 0;
+    float width = 0;
+    float area = 0;
+
+    cout << "Enter the length of the room: ";
+    getline(cin, sLength);
+    stringstream(sLength) >> length;
+
+    cout << "Enter width: ";
+    getline(cin, sWidth);
+    stringstream(sWidth) >> width;
+
+    area = length * width;
+    cout << "\nThe area of the room is: " << area << endl;
+
+    return 0;
 }
