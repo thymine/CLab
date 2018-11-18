8bbf18f232d191c41eb58dea68ddb12792c63f9f
zhangxiangdong <zxdeinstein@163.com>
2018-11-18 17:11:21 +0800

    Use switch statement

--- c/cards.c
+++ c/cards.c
@@ -10,17 +10,26 @@ int main() {
 	// fflush(stdout);
 	scanf("%2s", card_name);
 	int val = 0;
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;		
-	} else {
-		val = atoi(card_name);
+	switch(card_name[0]) {
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		default:
+			val = atoi(card_name);
+			break;
 	}
+
+	if (val >= 3 && val <= 6) {
+		puts("count++");
+	} else if (val == 10) {
+		puts("count--");
+	}
+
 	printf("Card points is: %d\n", val);
 	return 0;
 }
