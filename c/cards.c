dd5097c0c3d15c80e93f5adfcbd35aee589036b9
zhangxiangdong <zxdeinstein@163.com>
2018-11-18 17:35:12 +0800

    Loop your work

--- c/cards.c
+++ c/cards.c
@@ -3,33 +3,42 @@
 
 int main() {
 
-	setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-	char card_name[3];
-	puts("Input card name: ");
-	// fflush(stdout);
-	scanf("%2s", card_name);
-	int val = 0;
-	switch(card_name[0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		default:
-			val = atoi(card_name);
-			break;
-	}
+    char card_name[3];
+    int count = 0;
+    do {
+        puts("Input card name: ");
+        // fflush(stdout);
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if (val < 1 || val > 10) {
+                    puts("INVALID INPUT [1, 10]");
+                    continue;
+                }
+                break;
+        }
 
-	if (val >= 3 && val <= 6) {
-		puts("count++");
-	} else if (val == 10) {
-		puts("count--");
-	}
+        if (val >= 3 && val <= 6) {
+            count++;
+        } else if (val == 10) {
+            count--;
+        }
 
-	printf("Card points is: %d\n", val);
-	return 0;
+        printf("Current count: %d\n", count);
+    } while (card_name[0] != 'X');
+    return 0;
 }
